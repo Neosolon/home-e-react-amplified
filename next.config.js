1be0ba7b9e485ab283c2c1e658ce09e5dd01a2b9
Daniel F..Huertas <engcxr.dhuertas@gmail.com>
2023-05-12 01:47:44 -0700

    [BUILDFIX]

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, //change to true when not in development
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig
